e602f9bba889766c11e6f935eb09b97d28dbddbe
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-06 00:05:54 +0100

    mock data added

--- BucketList/Goal.swift
+++ BucketList/Goal.swift
@@ -22,3 +22,18 @@ class Goal {
     self.completedOn = completedOn
   }
 }
+
+extension Goal {
+  @MainActor
+  static var preview: ModelContainer {
+    guard let container = try? ModelContainer(for: Self.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true)) else {
+      fatalError("Failed to create preview container!")
+    }
+    container.mainContext.insert(Goal(title: "Dive the Great Barrier Reef", notes: "Need scuba certification first ", completed: false))
+    container.mainContext.insert(Goal(title: "Become Scuba Certified", notes: "East Coast Divers is in Brookline, MA that can do this", completed: true))
+    container.mainContext.insert(Goal(title: "Hike the Great Wall", notes: "Maybe during study abroad", completed: true))
+    container.mainContext.insert(Goal(title: "Safari in Kenya", notes: "Try to schedule something during the Great Migration", completed: false))
+    container.mainContext.insert(Goal(title: "Hike Machu Picchu", notes: "Learning to surf in Lima might also be cool", completed: false))
+    return container
+  }
+}
