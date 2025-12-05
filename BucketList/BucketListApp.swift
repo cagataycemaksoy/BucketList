c17098d0f363abcafb29b893b034180f3443d31d
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-05 23:05:42 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Cem Aksoy on 5.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
