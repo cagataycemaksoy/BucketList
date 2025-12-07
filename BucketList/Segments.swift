07c9edfa4f2de83fb9e413a609c2b43c0b3a3e93
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-12-07 13:43:20 +0100

    Segments enum added

---
+++ BucketList/Segments.swift
@@ -0,0 +1,14 @@
+//
+//  Segments.swift
+//  BucketList
+//
+//  Created by Cem Aksoy on 7.12.2025.
+//
+
+import Foundation
+
+enum Segments: String, CaseIterable  {
+  case all = "All"
+  case completeed = "Completed"
+  case open = "Open"
+}
